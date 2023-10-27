669d74c37f551d12afcb6115952b73c508a4b4f2
pedro.lima <pedro.henrique.araujo.lima@academico.ufpb.br>
2023-10-26 22:00:09 -0300

    Create MyLock.java

---
+++ ExclusaoMutua/Algoritmo_Dekker/MyLock.java
@@ -0,0 +1,25 @@
+package Algoritmo_Dekker;
+
+public class MyLock extends Thread{
+    Lock lock;
+    int id;
+    public MyLock(Lock lock, int id){
+        this.lock = lock;
+        this.id = id;
+    }
+    public void run(){
+        lock.requestCS(id);
+        System.out.println("Thread "+id+" entrou na secao critica");
+        lock.releaseCS(id);
+        System.out.println("Thread "+id+" saiu da secao critica");
+    }
+    
+    public static void main(String[] args) {
+        Lock lock = new LockImpl();
+        System.out.println("Testando exclusao mutua com Algoritmo de Dekker");
+        MyLock t0 = new MyLock(lock,0);
+        MyLock t1 = new MyLock(lock,1);
+        t0.start();
+        t1.start();
+    }
+}
